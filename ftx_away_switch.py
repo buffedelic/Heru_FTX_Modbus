d3fc4b8bef1d0aa0d53e9703098d1725b61a56da
buffedelic <buffedelic@hotmail.com>
2020-12-09 18:39:05 +0100

    Python 3 compability

--- ftx_away_switch.py
+++ ftx_away_switch.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -
 
 import minimalmodbus
@@ -7,10 +7,7 @@ import os
 import time
 import RPi.GPIO as GPIO
 
-minimalmodbus.CLOSE_PORT_AFTER_EACH_CALL = True
-minimalmodbus.STOPBITS = 2
-
-instr = minimalmodbus.Instrument('/dev/ttyUSB0', 4) #port, slaveadress
+instr = minimalmodbus.Instrument('/dev/ttyUSB0', 1) #port, slaveadress
 instr.precalculate_read_size = False
 #instr.debug = True
 
@@ -23,13 +20,13 @@ def set_away(state):
 		if read_status() == 0:
 			switch_status(0)
 		else:
-			print "No change in state"
+			print("No change in state")
 			pass
 	elif state == "off":
 		if read_status() == 1:
 			switch_status(1)
 		else:
-			print "No change in state"
+			print("No change in state")
 			pass
 
 def read_status(register=3):
@@ -37,7 +34,7 @@ def read_status(register=3):
 	status = None
 	while status is None:
 		try:
-			status = instr.read_bit(register, functioncode=1) #läs av status för bortaläge
+			status = instr.read_bit(register, functioncode=1) #Read state if away
 			print("Away mode is {}".format(statusstr[status]))
 			return status
 		except:
@@ -52,7 +49,7 @@ def switch_status(status):
 	done = False
 	while not done:
 		try:
-			instr.write_bit(3, switched_status, functioncode=5) #Switcha status mellan av/på
+			instr.write_bit(3, switched_status, functioncode=5) #Switch between on/off
 			done = True
 			print("Awaymode switched to {}".format(statusstr[switched_status]))
 		except:
@@ -65,10 +62,8 @@ def RCtime (pin=10):
 	time.sleep(0.1)
 
 	GPIO.setup(pin, GPIO.IN)
-	# This takes about 1 millisecond per loop cycle
 
 	while (GPIO.input(pin) == GPIO.LOW):
-	    #print "low"
 	    reading += 1
 	return reading
 
@@ -90,21 +85,20 @@ if __name__ == '__main__':
 		print l[i]
 		i = i + 1
 		if time.time() > timeout:
-			#print "broke while loop"
 			break
 	average = sum(l) / float(len(l))
 	print("Average: {}".format(average))
 
 	if average > home:
 		set_away("off")
-		print "Alarm deactivated"
+		print("Alarm deactivated")
 	elif average <= away:
 		set_away("on")
-		print "Alarm activated"
+		print("Alarm activated")
 	elif away <= average <= home_protection:
 		set_away("off")
-		print "Home protection activated"
-	print "Done"
+		print("Home protection activated")
+	print("Done")
 
 
 GPIO.cleanup(10)
